297075802dc1b921ac77093d044b076ca39a6d51
Jyotiranjan767 <jyotiranjan767@gmail.com>
2021-10-29 14:18:37 +0530

    dto class and user controller service added

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "./schemas/user.schema";
+import { UserController } from "./user.controller";
+import { UserRepository } from "./user.repository";
+import { UserService } from "./user.service";
 
 @Module({
-  imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])]
+  imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
+  controllers: [UserController],
+  providers: [UserService, UserRepository]
 })
 export class UserModule{}
